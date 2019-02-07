f6f84ae12038ebd592f5f921adc4f447f063b05b
Jose M Ramirez <josemramirez81@outlook.com>
2019-02-07 17:08:42 -0500

    Adding React-Router and Create a new Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
 import AddTodo from './components/AddTodo';
 import uuid from 'uuid';
 
@@ -55,13 +57,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header/>
-          <AddTodo addTodo={this.addTodo}/>
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header/>
+            <Route exact path="/" render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo}/>
+                  <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+                </React.Fragment>
+            )}/>
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
